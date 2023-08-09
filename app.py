cee74c9b62eb27e248f02377b719f0d10d191293
oppo870814870814 <138690771+oppo870814870814@users.noreply.github.com>
2023-08-09 14:15:40 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def handle_message(event):
     
     # 新增使用者關注的股票到mongodb
     if re.match('關注[0-9]{4}[<>][0-9]' , msg): #使用者新增股票至股票清單
-        stockNumber = msg[2:6]
+        stockNumber = msg[2:]
         line_bot_api.push_message(uid , TextSendMessage('加入股票代號'+stockNumber))
         content = write_my_stock(uid , user_name , stockNumber , msg[6:7] , msg[7:])
         line_bot_api.push_message(uid , TextSendMessage(content))
