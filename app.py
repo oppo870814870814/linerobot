57525075df1c2c2ac54c9b0eafe870114b94cb56
oppo870814870814 <138690771+oppo870814870814@users.noreply.github.com>
2023-08-09 13:55:07 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from line_bot_api import *
 from events.basic import *
 from events.oil import *
 from events.Msg_Template import *
-from model.MongoDB import *
+from model.mongodb import *
 import re
 import twstock
 import datetime
@@ -64,13 +64,14 @@ def handle_message(event):
     # 新增使用者關注的股票到mongodb
     if re.match('關注[0-9]{4}[<>][0-9]' , msg): #使用者新增股票至股票清單
         stockNumber = msg[2:6]
+        line_bot_api.push_message(uid , TextSendMessage('加入股票代號'+stockNumber))
         content = write_my_stock(uid , user_name , stockNumber , msg[6:7] , msg[7:])
         line_bot_api.push_message(uid , TextSendMessage(content))
     
-    else:
-        content = write_my_stock(uid , user_name , stockNumber , "未設定" , "未設定")
-        line_bot_api.push_message(uid , TemplateSendMessage(content))
-        return 0
+    # else:
+    #     content = write_my_stock(uid , user_name , stockNumber , "未設定" , "未設定")
+    #     line_bot_api.push_message(uid , TemplateSendMessage(content))
+    #     return 0
         
     if (emsg.startswith('#')):
         text = emsg[1:]
