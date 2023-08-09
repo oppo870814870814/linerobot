a18a8e336626943ada854f77512773006413c62e
oppo870814870814 <138690771+oppo870814870814@users.noreply.github.com>
2023-08-09 10:22:03 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def handle_message(event):
 
     #股價查詢
     if re.match("想知道股價[0-9]" , msg):
-        stockNumber = msg[2:6]
+        stockNumber = msg[5:9]
         btn_msg = stock_reply_other(stockNumber)
         line_bot_api.push_message(uid , btn_msg)
         return 0
