0de971b8a2f8c74cf5dcb403b00acad8079f9770
oppo870814870814 <138690771+oppo870814870814@users.noreply.github.com>
2023-08-11 15:07:34 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -63,125 +63,117 @@ def handle_message(event):
         )
 
     # ############################"股票區"###########################
-    if message_text == "股價查詢":
-        line_bot_api.push_message(
-        uid, 
-        TextSendMessage("請輸入'#' + '股票代號'\n範例：#2330")
-        )
-
-    #股價查詢
-    if re.match("想知道股價[0-9]" , msg):
-        stockNumber = msg[5:]
-        btn_msg = stock_reply_other(stockNumber)
-        line_bot_api.push_message(uid , btn_msg)
-        return 0
-    
-    # 新增使用者關注的股票到mongodb
-    if re.match('關注[0-9]{4}[<>][0-9]' ,msg): # 使用者新增股票至股票清單
-        tockNumber = msg[2:6]
-        line_bot_api.push_message(uid, TextSendMessage("加入股票代號"+stockNumber))
-        content = write_my_stock(uid, user_name , stockNumber, msg[6:7], msg[7:])
-        line_bot_api.push_message(uid, TextSendMessage(content))
-        return 0
-       
     
     # 查詢股票篩選條件清單
-    if re.match('股票清單' , msg):
-        line_bot_api.push_message(uid , TextSendMessage('請稍等一下 , 股票查詢中...'))
-        content = show_stock_setting(user_name , uid)
-        line_bot_api.push_message(uid , TextSendMessage(content))
-        return 0
+    if event.message.text == "股價查詢":
+        line_bot_api.push_message(uid,TextSendMessage("請輸入#加股票代號....."))
     
-    # 珊除存在資料庫裡面的股票
-    if re.match('刪除[0-9]{4}' , msg):
-        content = delete_my_stock(user_name , msg[2:])
-        line_bot_api.push_message(uid , TextSendMessage(content))
+    #股價查詢
+    if re.match("想知道股價[0-9]", msg):
+        msg = msg[5:]
+        btn_msg = stock_reply_other(msg)
+        line_bot_api.push_message(uid, btn_msg)
         return 0
-    
+    #新增使用者關注的股票到mongodb
+    if re.match('關注[0-9]{4}[<>][0-9]' ,msg): # 使用者新增股票至股票清單
+       stockNumber = msg[2:6]
+       line_bot_api.push_message(uid, TextSendMessage("加入股票代號"+stockNumber))
+       content = write_my_stock(uid, user_name , stockNumber, msg[6:7], msg[7:])
+       line_bot_api.push_message(uid, TextSendMessage(content))
+       return 0
+    # 查詢股票篩選條件清單
+    if re.match('股票清單',msg): 
+        line_bot_api.push_message(uid, TextSendMessage('稍等一下, 股票查詢中...'))
+        content = show_stock_setting(user_name, uid)
+        line_bot_api.push_message(uid, TextSendMessage(content))
+        return 0
+    # 刪除存在資料庫裡面的股票
+    if re.match('刪除[0-9]{4}',msg): 
+        content = delete_my_stock(user_name, msg[2:])
+        line_bot_api.push_message(uid, TextSendMessage(content))
+        return 0
     # 清空存在資料庫裡面的股票
-    if re.match('清空股票' , msg) :
-        content = delete_my_allstock(user_name , uid)
-        line_bot_api.push_message(uid , TextSendMessage(content))
+    if re.match('清空股票',msg): 
+        content = delete_my_allstock( user_name, uid)
+        line_bot_api.push_message(uid, TextSendMessage(content))
         return 0
-    
-    if (emsg.startswith('#')):
-        text = emsg[1:]
-        content =''
+    if(msg.startswith('#')):
+            text = msg[1:]
+            content = ''
 
-        stock_rt = twstock.realtime.get(text)
-        my_datetime = datetime.datetime.fromtimestamp(stock_rt['timestamp']+8*60*60)
-        my_time = my_datetime.strftime('%H:%M:%S')
+            stock_rt = twstock.realtime.get(text)
+            my_datetime = datetime.datetime.fromtimestamp(stock_rt['timestamp']+8*60*60)
+            my_time = my_datetime.strftime('%H:%M:%S')
 
-        content +='%s (%s) %s\n' % (
-            stock_rt['info']['name'],
-            stock_rt['info']['code'],
-            my_time)
-        
-        content += '現價: %s / 開盤: %s\n'%(
-            stock_rt['realtime']['latest_trade_price'],
-            stock_rt['realtime']['open'])
-        
-        content += '最高: %s / 最低:%s\n'%(
-            stock_rt['realtime']['high'],
-            stock_rt['realtime']['low'])
-        
-        content += '量: %s\n'%(stock_rt['realtime']['accumulate_trade_volume'])
+            content += '%s (%s) %s\n' %(
+                stock_rt['info']['name'],
+                stock_rt['info']['code'],
+                my_time)
+            content += '現價: %s / 開盤: %s\n'%(
+                stock_rt['realtime']['latest_trade_price'],
+                stock_rt['realtime']['open'])
+            content += '最高: %s / 最低: %s\n' %(
+                stock_rt['realtime']['high'],
+                stock_rt['realtime']['low'])
+            content += '量: %s\n' %(stock_rt['realtime']['accumulate_trade_volume'])
 
-        stock = twstock.Stock(text)
-        content += '-----\n'
-        content += '最近五日價格: \n'
-        price5 = stock.price[-5:][::-1]
-        date5 = stock.date[-5:][::-1]
-        for i in range(len(price5)):
-            content += '[%s] %s\n' % (date5[i].strftime("%Y-%m-%d"), price5[i])
-        line_bot_api.reply_message(
-            event.reply_token, 
-            TextSendMessage(text=content))
+            stock = twstock.Stock(text)#twstock.Stock('2330')
+            content += '-----\n'
+            content += '最近五日價格: \n'
+            price5 = stock.price[-5:][::-1]
+            date5 = stock.date[-5:][::-1]
+            for i in range(len(price5)):
+                #content += '[%s] %s\n' %(date5[i].strftime("%Y-%m-%d %H:%M:%S"), price5[i])
+                content += '[%s] %s\n' %(date5[i].strftime("%Y-%m-%d"), price5[i])
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=content)
+            )
 
 ############################股價提醒######################
-    if re.match("股價提醒" , msg):
+    if re.match("股價提醒", msg):
         import schedule
         import time
-    # 查看當前股價
-    def look_stock_price(stock , condition , price , userID) :
-        print(userID)
-        url = 'https://tw.stock.yahoo.com/q/q?s=' + stock
-        list_req = requests.get(url)
-        soup = BeautifulSoup(list_req.content , 'html.paper')
-        getstock = soup.findAll('b')[1].text
-        content = stock + '當前股市價格為 : ' + getstock
-        if condition == '<':
-            content += "\n篩選條件為: < "+ price
-            if float(getstock) < float(price):
-                content += '\n符合' + getstock + ' < ' + price + '的篩選條件'
-                line_bot_api.push_message(userID , TextSendMessage(text = content))
-        elif condition == '>':
-            content += "\n篩選條件為: < "+ price
-            if float(getstock) < float(price):
-                content += '\n符合' + getstock + ' > ' + price + '的篩選條件'
-                line_bot_api.push_message(userID , TextSendMessage(text = content))
-        elif condition == '=':
-            content += "\n篩選條件為: < "+ price
-            if float(getstock) == float(price):
-                content += '\n符合' + getstock + ' = ' + price + '的篩選條件'
-                line_bot_api.push_message(userID , TextSendMessage(text = content))
-    def job():
-        print('HH')
-        dataList = cache_users_stock()
-        #print(dataList)
-        for i in range(len(dataList)):
-            for k in range(len(dataList[i])):
-                #print(dataList[i][k])
-                look_stock_price(dataList[i][k]['favorite_stock'], dataList[i][k]['condition'], dataList[i][k]['price'], dataList[i][k]['userID'])
-    schedule.every(30).seconds.do(job).tag("daily-tasks-stock"+uid,'second') #每10秒執行一次
-    #schedule.every().hour.do(job) #每小時執行一次
-    # schedule.every().day.at('17:19').do(job)  #每天9.30執行一次
-    # schedule.every().monday.do(job) #美洲執行一次
-    # schedule.every().wednesday.at(14:45).do(job) #每周三14.45執行一次
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+        # 查看當前股價
+        def look_stock_price(stock, condition, price, userID):
+            print(userID)
+            url = 'https://tw.stock.yahoo.com/q/q?s=' + stock
+            list_req = requests.get(url)
+            soup = BeautifulSoup(list_req.content, "html.parser")
+            getstock= soup.findAll('b')[1].text
+            content = stock + "當前股市價格為: " +  getstock
+            if condition == '<':
+                content += "\n篩選條件為: < "+ price
+                if float(getstock) < float(price):
+                    content += "\n符合" + getstock + " < " + price + "的篩選條件"
+                    line_bot_api.push_message(userID, TextSendMessage(text=content))
+            elif condition == '>':
+                content += "\n篩選條件為: > "+ price
+                if float(getstock) > float(price):
+                    content += "\n符合" + getstock + " > " + price + "的篩選條件"
+                    line_bot_api.push_message(userID, TextSendMessage(text=content))
+            elif condition == "=":
+                content += "\n篩選條件為: = "+ price
+                if float(getstock) == float(price):
+                    content += "\n符合" + getstock + " = " + price + "的篩選條件"
+                    line_bot_api.push_message(userID, TextSendMessage(text=content))
+        def job():
+            print('HH')
+            dataList = cache_users_stock()
+            # print(dataList)
+            for i in range(len(dataList)):
+                for k in range(len(dataList[i])):
+                    # print(dataList[i][k])
+                    look_stock_price(dataList[i][k]['favorite_stock'], dataList[i][k]['condition'], dataList[i][k]['price'], dataList[i][k]['userID'])
+        schedule.every(30).seconds.do(job).tag('daily-tasks-stock'+uid,'second') #每10秒執行一次
+        #schedule.every().hour.do(job) #每小時執行一次
+        #schedule.every().day.at("17:19").do(job) #每天9點30執行一次
+        #schedule.every().monday.do(job) #每週一執行一次
+        #schedule.every().wednesday.at("14:45").do(job) #每週三14點45執行一次
+        # 無窮迴圈
+        while True: 
+            schedule.run_pending()
+            time.sleep(1)
 
     #####################################匯率區#######################################
     if re.match('幣別種類' , emsg):
